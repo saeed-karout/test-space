3783e4b2113b3c8e6e5a0520de1d4625e494b214
saeed-karout <saed.karot1@gmail.com>
2024-09-08 11:04:41 +0300

    update

--- src/components/KitchenSpace.jsx
+++ src/components/KitchenSpace.jsx
@@ -13,7 +13,7 @@ const Faucet = () => {
     <div className="h-screen w-screen relative">
       {/* تعيين الصورة image1 كخلفية */}
       <div
-        className="absolute inset-0 bg-cover bg-center"
+        className="absolute inset-0 bg-contain bg-center"
         style={{
           backgroundImage: `url(${faucetImage})`, // صورة الحنفية كخلفية
         }}
@@ -23,15 +23,15 @@ const Faucet = () => {
           src={selectedImage}
           alt="Selected"
           className="absolute w-auto h-auto object-contain z-40"
-          style={{ top: '55%', left: '50%', transform: 'translate(-50%, -50%)', maxWidth: '50%', maxHeight: '50%' }}
+          style={{ top: '52%', left: '50%', transform: 'translate(-50%, -50%)', maxWidth: '50%', maxHeight: '50%' }}
         />
       </div>
 
       <div className="absolute right-0 top-[40%] transform -translate-x-1/2">
-        <label className="mr-2 mx-auto font-bold text-white">Select Items:</label>
 
         {/* خيارات الصور عبر div مع ألوان قابلة للنقر */}
-        <div className="flex flex-col space-y-4 mt-2">
+        <div className="flex flex-col space-y-4 mt-2 items-center">
+        <label className=" mx-auto font-bold text-white">Select Items:</label>
           <div
             onClick={() => setSelectedImage(image1)}
             className="w-12 h-12 rounded-full cursor-pointer"
